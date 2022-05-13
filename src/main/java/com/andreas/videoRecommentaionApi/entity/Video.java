a4caa34ceee25bd6f900bc129a8b26cd45eff3ca
mouzong <andreasmouzong@gmail.com>
2022-05-13 11:23:42 +0100

    finished the integration test for getting all videos from DB

--- src/main/java/com/andreas/videoRecommentaionApi/entity/Video.java
+++ src/main/java/com/andreas/videoRecommentaionApi/entity/Video.java
@@ -1,11 +1,17 @@
 package com.andreas.videoRecommentaionApi.entity;
 
+import lombok.*;
 import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 @Entity
 @Table(name = "videos")
 public class Video {
@@ -24,37 +30,4 @@ public class Video {
      @ElementCollection(targetClass = String.class)
      protected List<String> labels;
 
-     public Video() {
-     }
-
-     public Video(String id, String title, List<String> labels) {
-          this.id = id;
-          this.title = title;
-          this.labels = labels;
-     }
-
-
-     public String getId() {
-          return id;
-     }
-
-     public void setId(String id) {
-          this.id = id;
-     }
-
-     public String getTitle() {
-          return title;
-     }
-
-     public void setTitle(String title) {
-          this.title = title;
-     }
-
-     public List<String> getLabels() {
-          return labels;
-     }
-
-     public void setLabels(List<String> labels) {
-          this.labels = labels;
-     }
 }
--- src/test/java/com/andreas/videoRecommentaionApi/VideoRecommendationApiApplicationTests.java
+++ src/test/java/com/andreas/videoRecommentaionApi/VideoRecommendationApiApplicationTests.java
@@ -1,13 +1,46 @@
 package com.andreas.videoRecommentaionApi;
 
+import com.andreas.videoRecommentaionApi.entity.Video;
+import com.andreas.videoRecommentaionApi.repository.VideoRepository;
+import org.hamcrest.CoreMatchers;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.List;
+import java.util.UUID;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class VideoRecommendationApiApplicationTests {
+	@Autowired
+	private VideoRepository videoRepository;
 
+	@Autowired
+	private MockMvc mockMvc;
+
+	// given/when/then format BDD style
 	@Test
-	void contextLoads() {
+	public void givenVideos_whenGetAllVideo_thenListOfVideos() throws Exception {
+		// given - setup precondition
+		List<Video> videos = List.of(
+				Video.builder().id(String.valueOf(UUID.randomUUID())).title("No time to die").labels(List.of("James Bond", "007")).build(),
+				Video.builder().id(String.valueOf(UUID.randomUUID())).title("Dr Mobius").labels(List.of("Action ", "Horror")).build(),
+				Video.builder().id(String.valueOf(UUID.randomUUID())).title("Dr House").labels(List.of("Hospital", "Sorrow")).build()
+		);
+		videoRepository.saveAll(videos);
+
+		// when - action
+		ResultActions response = mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/videos"));
+
+		// then - verify output
+		response.andExpect(MockMvcResultMatchers.status().isOk());
+		response.andExpect(MockMvcResultMatchers.jsonPath("$.size()", CoreMatchers.is(videos.size())));
 	}
 
 }
