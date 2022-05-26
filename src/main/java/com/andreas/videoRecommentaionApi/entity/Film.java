5390220f2e780cd6765a86fa5f9c217ac6ab31ed
mouzong <andreasmouzong@gmail.com>
2022-05-26 10:39:28 +0100

    added the Film and Serie entities

--- src/main/java/com/andreas/videoRecommentaionApi/entity/Film.java
+++ src/main/java/com/andreas/videoRecommentaionApi/entity/Film.java
@@ -4,13 +4,14 @@ import lombok.*;
 
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 @Entity
-@DiscriminatorValue("Serie")
-public class Serie  extends  Video{
-    private String author;
+@DiscriminatorValue("Film")
+public class Film extends  Video{
+    private String actor;
 }
