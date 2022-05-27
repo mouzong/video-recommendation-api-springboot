ca0cb505c0658f7e799c485d7ac3cc1e23013892
mouzong <andreasmouzong@gmail.com>
2022-05-27 04:02:26 +0100

    adding the videoDTO

--- src/main/java/com/andreas/videoRecommentaionApi/dto/VideoDto.java
+++ src/main/java/com/andreas/videoRecommentaionApi/dto/VideoDto.java
@@ -1,5 +1,11 @@
 package com.andreas.videoRecommentaionApi.dto;
 
 
+import java.util.List;
+import java.util.UUID;
+
 public class VideoDto {
+    private UUID videoId;
+    private String title;
+    private List<String> labels;
 }
