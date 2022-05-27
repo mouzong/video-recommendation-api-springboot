429e68bf3cb241709c5de029ca15b679348fd534
mouzong <andreasmouzong@gmail.com>
2022-05-27 05:54:10 +0100

    adding the VideoEntity to and fro VideoDTO

--- src/main/java/com/andreas/videoRecommentaionApi/service/VideoServiceImpl.java
+++ src/main/java/com/andreas/videoRecommentaionApi/service/VideoServiceImpl.java
@@ -1,7 +1,30 @@
 package com.andreas.videoRecommentaionApi.service;
 
+import com.andreas.videoRecommentaionApi.dto.VideoDto;
+import com.andreas.videoRecommentaionApi.entity.Video;
+import com.andreas.videoRecommentaionApi.repository.VideoRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class VideoServiceImpl implements VideoService{
+
+    @Autowired
+    private VideoRepository videoRepository;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public VideoDto convertVideoEntityToVideoDto(Video video){
+        VideoDto mappedVideoDto = new VideoDto();
+        mappedVideoDto = modelMapper.map(video, VideoDto.class);
+        return mappedVideoDto;
+    }
+
+    public Video convertVideoDtoToVideoEntity(VideoDto videoDto){
+        Video mappedVideoEntity = new Video();
+        mappedVideoEntity = modelMapper.map(videoDto, Video.class);
+        return mappedVideoEntity;
+    }
 }
